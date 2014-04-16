a5fd86cfcbfb15f38c8e434c8f436fcc5618d9f2
puhrome <phirom.yim@fullsail.edu>
2014-04-16 17:10:43 -0400

    Conditional Else statement
    Using else in place of two if statements in WebStorm.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Conditional Logic Created by Phirom Yim on 4/16/14.
+ */
+//Else Statement
+
+var kidHeight = 47;
+var minHeight = 48;
+
+
+//if kid is old enough print to console
+//if kid is over 48 inches in height
+if( kidHeight > minHeight){
+    console.log("You can ride the coaster!");
+//code performed if condition is true
+}else{
+    console.log("You can ride the coaster!");
+    //code performed if condition is false
+}
+//Else takes place of this condition
+/*if(kidHeight <= minHeight){
+    console.log("Sorry kid you've got some growing to do first!");
+    //code perform if condition is false
+}*/
+
